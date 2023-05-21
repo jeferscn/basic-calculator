7e776bf49742ce614edc0f1c6e9a86e0170dbee5
Jeferson Pereira <jefe_ads@hotmail.com>
2023-05-21 18:06:05 -0300

    set zero value as first value when open app

--- app/src/main/java/com/example/basiccalculator/MainActivity.kt
+++ app/src/main/java/com/example/basiccalculator/MainActivity.kt
@@ -18,6 +18,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setCalculatorData(calculatorData)
+
         binding.buttonCalculatorClear.setOnClickListener {
             setCalculatorData(insertion = "0", isClearCall = true)
         }
